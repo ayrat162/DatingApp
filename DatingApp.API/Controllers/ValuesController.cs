e074d3d0a076305396b56761d0cac0f3de62f11f
Airat Musin <Airat.Musin@icl-services.com>
2020-05-10 02:13:33 +0300

    added automapper, userdtos

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -26,16 +26,16 @@ namespace DatingApp.API.Controllers
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
-            var values = _context.Values.ToListAsync();
-            return Ok(values.Result);
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
         }
 
         [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
-            var value = _context.Values.FirstOrDefaultAsync(x => x.Id == id);
-            return Ok(value.Result);
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(value);
         }
     }
 }
